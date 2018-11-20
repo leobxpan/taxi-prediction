fe155ae592e6045017589ec0e39ee5806e7ce1ef
Leo <pankobe24@gmail.com>
2018-11-20 08:02:50 +0000

    Current state of our project. Just before milestone deadline.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import argparse
 import os
+import pdb
 import time
 import torch
 import shutil
 
+from taxiDataset import *
+
+best_loss = np.Inf
+
 def main():
+    
+    global best_loss, args
+    
     parser = argparse.ArgumentParser(description="Main file for taxi prediction")
     
     # ========================= Model Configs ========================== 
     parser.add_argument('--dropout', type=float, default=0.5)
-    parser.add_argument('--loss', type=str, default='mse', choices=['mse', 'mae'])
+    parser.add_argument('--loss', type=str, default='mae', choices=['mse', 'mae'])
 
     # ========================= Learning Configs ==========================
     parser.add_argument('--epochs', type=int, default=50)
@@ -18,21 +26,24 @@ def main():
     parser.add_argument('--lr_steps', type=int, default=[20, 40], help='epochs to decay learning rate by 10')
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight-decay', type=float, default=5e-4) 
+
     # ========================= Monitor Configs ==========================
     parser.add_argument('--print-freq', type=int, default=10)
     parser.add_argument('--eval-freq', type=int, default=5)
 
     # ========================= Runtime Configs ==========================
+    parser.add_argument('--val', type=bool, default=False)
     parser.add_argument('--resume', type=str, default='')
     parser.add_argument('--start-epoch', type=int, default=0)
-    parser.add_argument('iter_p_epoch', type=int, default=500)
+    parser.add_argument('--iter_p_epoch', type=int, default=500)
 
     args = parser.parse_args()
 
     # Model architecture params
     feature_num = 3
-    H1 = 5
-    H2 = 5
+    H1 = 4 
+    H2 = 6 
+    H3 = 5
     output_dim = 1
 
     # Model configuration
@@ -40,60 +51,94 @@ def main():
         torch.nn.Linear(feature_num, H1),
         torch.nn.ReLU(),
         torch.nn.Linear(H1, H2),
-        torch.nn.Relu(),
+        torch.nn.ReLU(),
+        #torch.nn.Linear(H2, H3),
+        #torch.nn.ReLU(),
         torch.nn.Linear(H2, output_dim),
-        torch.nn.Relu()
-    )
-
-    # Load checkpoint, if any
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print(("=> loading checkpoint '{}'".format(args.resume)))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_loss = checkpoint['best_loss']
-            model_state_dict = checkpoint['model_state_dict']
-
-            model.load_state_dict(model_state_dict)
-            print(("=> loaded checkpoint (epoch {})"
-                .format(checkpoint['epoch'])))
-        else:
-            print(("=> no checkpoint found at '{}'".format(args.resume)))
+        torch.nn.ReLU(),
+    ).cuda()
+   
+    # Parameter Initialization
+    model.apply(init_weights)
 
     # Load data
-    train_loader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, pin_memory=True)
-    val_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, pin_memory=True)
-    test_loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, pin_memory=True)
-    
-    # Optimization
-    if args.loss == 'mse':
-        loss = torch.nn.MSELoss(size_average=True)
-    elif args.loss == 'mae':
-        loss = torch.nn.L1Loss(size_average=True)
-    else:
-        raise ValueError("Only MSE and MAE are supported while we got {}".format(args.loss))
+    dl = ['PULocationID','day_of_week','t_bucket']
+    taxi_dataset = taxiDataset(csv_file='yellow_tripdata_2016-12.csv', desired_labels=dl)#, length=4000)
+    train_loader = torch.utils.data.DataLoader(taxi_dataset.train, batch_size=args.batch_size, shuffle=True, pin_memory=True)
+    print(len(taxi_dataset))
+    val_loader = torch.utils.data.DataLoader(taxi_dataset.val, batch_size=1, shuffle=True, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(taxi_dataset.test, batch_size=1, shuffle=True, pin_memory=True)
 
-    optimizer = torch.optim.SGD(model.parameters, args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+    if args.val:
+        if args.resume:
+            if os.path.isfile(args.resume):
+                print(("=> loading checkpoint '{}'".format(args.resume)))
+                checkpoint = torch.load(args.resume)
+                model_state_dict = checkpoint['model_state_dict']
 
-    for epoch in range(args.start_epoch, args.epochs):
-        # Evaluation on validation set
-        if epoch % args.eval_freq == 0 or epoch == args.epochs - 1:
-            loss = validate(val_loader, model)
-            
-            is_best = loss > best_loss
-            best_loss = min(loss, best_loss)
+                model.load_state_dict(model_state_dict)
+                print("=> loaded checkpoint")
+                
+                validate(test_loader, model)
+            else:
+                raise ValueError(("=> no checkpoint found at '{}'".format(args.resume)))
+        else:
+            raise ValueError("Lack trained model as input. Use --resume")
+    else:          
+        # Load checkpoint, if any
+        if args.resume:
+            if os.path.isfile(args.resume):
+                print(("=> loading checkpoint '{}'".format(args.resume)))
+                checkpoint = torch.load(args.resume)
+                args.start_epoch = checkpoint['epoch']
+                best_loss = checkpoint['best_loss']
+                model_state_dict = checkpoint['model_state_dict']
 
-            print("Accuracy on validation set for epoch {} is {}%. Current best is {}%".format(epoch, loss, best_loss))
+                model.load_state_dict(model_state_dict)
+                print(("=> loaded checkpoint (epoch {})"
+                    .format(checkpoint['epoch'])))
+            else:
+                print(("=> no checkpoint found at '{}'".format(args.resume)))
 
-            save_checkpoint({
-                'epoch' : epoch + 1,
-                'model_state_dict' : model.state_dict(),
-                'best_loss' : best_loss,
-            }, is_best)
+        # Optimization
+        if args.loss == 'mse':
+            loss = torch.nn.MSELoss(size_average=True)
+        elif args.loss == 'mae':
+            loss = torch.nn.L1Loss(size_average=True)
+        else:
+            raise ValueError("Only MSE and MAE are supported while we got {}".format(args.loss))
 
-            adjust_learning_rate(optimizer, epoch, args.lr_steps)
-        
-            train(train_loader, val_loader, model, loss, optimizer, epoch, args.iter_p_epoch)
+        #optimizer = torch.optim.SGD(model.parameters(), args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+        optimizer = torch.optim.Adam(model.parameters())
+
+        # Print the mean of ground truth labels for comparison
+        print("The mean of ground truth labels is {}".format(loader_mean(val_loader)))
+
+        # Training
+        for epoch in range(args.start_epoch, args.epochs):
+            # Evaluation on validation set
+            if epoch % args.eval_freq == 0 or epoch == args.epochs - 1:
+                loss_val = validate(val_loader, model)
+
+                is_best = loss_val > best_loss
+                best_loss = min(loss_val, best_loss)
+
+                print("Loss on validation set for epoch {} is {}. Current minimum is {}".format(epoch, loss_val, best_loss))
+
+                save_checkpoint({
+                    'epoch' : epoch + 1,
+                    'model_state_dict' : model.state_dict(),
+                    'best_loss' : best_loss,
+                }, is_best)
+
+                #adjust_learning_rate(optimizer, epoch, args.lr_steps)
+
+            train(train_loader, model, loss, optimizer, epoch, args.iter_p_epoch)
+
+def init_weights(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.xavier_uniform(m.weight)
+        m.bias.data.fill_(0.0)
 
 def validate(val_loader, model):
     model.eval()
@@ -103,40 +148,38 @@ def validate(val_loader, model):
 
     with torch.no_grad():
         for i, (input_val, label) in enumerate(val_loader):
+            input_val = input_val.to(torch.float32).cuda()
             output = model(input_val)
             all_output.append(output.item())
             all_label.append(label.item())
 
-    loss = mse_loss(all_output, all_label)
+        loss = mse_loss(all_output, all_label)
 
-    print('Loss on validation set: {:.3f}'.format(loss))
+    print('Loss: {:.3f}'.format(loss))
+    
+    return loss
 
-def train(train_loader, val_loader, model, loss, optimizer, epoch, iter_p_epoch):
+def train(train_loader, model, loss, optimizer, epoch, iter_p_epoch):
     model.train()
 
     train_len = len(train_loader) - 1
-    val_len = len(val_loader) - 1
 
     for i in range(iter_p_epoch):
         if i % train_len == 0:
             if i != 0:
                 train_iter.next()
             train_iter = iter(train_loader)
-        if i % val_len == 0:
-            if i != 0:
-                val_iter.next()
-            val_iter = iter(val_loader)
 
         input_val, label = train_iter.next()
-        input_val = input_val.cuda()
-        label = label.cuda()
+        input_val = input_val.to(torch.float32).cuda()
+        label = label.to(torch.float32).cuda()
 
-        output = model(input_val)
+        output = model(input_val).view(-1)
 
-        loss = mse_loss(output, label)
+        backable_loss = loss(output, label)
 
         optimizer.zero_grad()
-        loss.backward()
+        backable_loss.backward()
 
         optimizer.step()
 
@@ -149,13 +192,17 @@ def train(train_loader, val_loader, model, loss, optimizer, epoch, iter_p_epoch)
         #        epoch, i, len(source_loader), batch_time=batch_time,
         #        data_time=data_time, c_loss=c_losses, ad_loss=ad_losses, lr=optimizer.param_groups[-1]['lr'])))
 
-#def mse_loss(output, label):
+def mse_loss(output, label):
+    return np.sum((np.asarray(output) - np.asarray(label)) ** 2) / len(output) 
+
+def mae_loss(output, label):
+    return np.sum(np.abs(np.asarray(output) - np.asarray(label))) / len(output)
 
 def save_checkpoint(state, is_best, file_name='checkpoint.pth.tar'):
-    torch.save(state, filename)
+    torch.save(state, file_name)
     if is_best:
         best_name = 'model_best.pth.tar'
-        shutil.copyfile(filename, bestname)
+        shutil.copyfile(file_name, best_name)
 
 def adjust_learning_rate(optimizer, epoch, lr_steps):
     decay = 0.1 ** (sum(epoch >= np.array(lr_steps)))
@@ -164,6 +211,18 @@ def adjust_learning_rate(optimizer, epoch, lr_steps):
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr * param_group['lr_mult']
         param_group['weight_decay'] = decay * param_group['decay_mult']
+
+def loader_mean(data_loader):
+    mean = 0
+    all_label = []
+
+    with torch.no_grad():
+        for i, (input_val, label) in enumerate(data_loader):
+            mean += label.item()
+            all_label.append(label)        
+        mean /= len(all_label)
+
+    return mean
 
 if __name__ == '__main__':
     main()
--- taxiDataset.py
+++ taxiDataset.py
@@ -1,7 +1,15 @@
+import os
+import torch
+import numpy as np
+import pandas as pd
+import datetime as dt
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
 class taxiDataset(Dataset):
     """Taxi Data dataset."""
     
-    def __init__(self, csv_file, root_dir, desired_labels, split):
+    def __init__(self, csv_file=None, root_dir=None, desired_labels=None, slot=30, length=None, split='all', data_frame=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -9,21 +17,53 @@ class taxiDataset(Dataset):
         """
         PU_dt = 'tpep_pickup_datetime'
         dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-        df_all = pd.read_csv(os.path.join('/home/lashi/assets', csv_file),usecols=np.arange(17),parse_dates=[PU_dt],date_parser=dateparse)
-        self.dayDict = self._createDayDict()
-        self.timeBuckets = self._createTimeBuckets()
-        self.df = self._parsetime(df_all, desired_labels, PU_dt, split)
-        self.root_dir = root_dir
         
+        self.dayDict = self._createDayDict()
+        self.timeBuckets = self._createTimeBuckets(slot)
+        self.train = None
+        self.val = None
+        self.test = None
+        self.root_dir = root_dir
+
+
+        if split == 'all':
+            csv_path = os.path.join('/home/lashi/assets', csv_file)
+            df_all = pd.read_csv(csv_path,usecols=np.arange(17),parse_dates=[PU_dt],date_parser=dateparse)
+            df_all = self._parsetime(df_all, desired_labels, PU_dt)
+        
+            if length == None:
+                length = len(df_all)
+
+            self.df = df_all.sample(frac=1).reset_index(drop=True)[:length]
+            self.train = taxiDataset(split='train',data_frame=self.df)
+            self.val = taxiDataset(split='val',data_frame=self.df)
+            self.test = taxiDataset(split='test',data_frame=self.df)  
+        elif split == 'test_only':
+            self.df = data_frame
+        else:
+            df_len = len(data_frame)
+            train_ind = int(0.9*df_len)
+            val_ind = int(0.5*(df_len-train_ind))+train_ind
+            if split == 'train':
+                self.df = data_frame[:train_ind]
+            elif split == 'val':
+                self.df = data_frame[train_ind:val_ind]
+            elif split == 'test':
+                self.df = data_frame[val_ind:]
+
+
     def __len__(self):
-        return len(self.data_frame)
+        return len(self.df)
     
     def __getitem__(self, idx):
         PU_loc = self.df.iloc[idx, 0]
-        PU_dow = self.dayDict[self.df.iloc[idx, 1]]
-        PU_tB = self.timeBuckets[self.df.iloc[idx, 2]]
+        PU_dow = self.df.iloc[idx, 1]
+        PU_tB = self.df.iloc[idx, 2]
+        # PU_dow = self.dayDict[self.df.iloc[idx, 1]]
+        # PU_tB = self.timeBuckets[self.df.iloc[idx, 2]]
         PU_num = self.df.iloc[idx, 3]
-        sample = {'PU_loc': PU_loc, 'PU_dow': PU_dow, 'PU_tB': PU_tB, 'PU_num': PU_num}
+        # sample = {'PU_loc': PU_loc, 'PU_dow': PU_dow, 'PU_tB': PU_tB, 'PU_num': PU_num}
+        sample = (np.array((PU_loc,PU_dow,PU_tB)),PU_num)	
 
         return sample
     
@@ -37,15 +77,14 @@ class taxiDataset(Dataset):
         
         return day_dict
     
-    def _createTimeBuckets(self):
+    def _createTimeBuckets(self, slot):
         # loop to create time bucket dictionary
         time_buckets = dict()
-        split = 30
-        numBuckets = int(24*(60/split))
+        numBuckets = int(24*(60/slot))
         ind = 0
         for hr in range(24):
-            for i in range(int(60/split)):
-                key = '%02d'%hr+':'+'%02d'%(i*split)+':00'
+            for i in range(int(60/slot)):
+                key = '%02d'%hr+':'+'%02d'%(i*slot)+':00'
                 oneHotVec = np.zeros((1,numBuckets)).astype('int')
                 oneHotVec[0,ind] = 1
                 time_buckets[key] = ind
@@ -54,7 +93,7 @@ class taxiDataset(Dataset):
                 
         return time_buckets
     
-    def _parsetime(self, data_frame, desired_labels, dt_label, split):
+    def _parsetime(self, data_frame, desired_labels, dt_label):
         data_frame[dt_label] = pd.to_datetime(data_frame[dt_label])
         data_frame[dt_label] = data_frame[dt_label].dt.round('30min')
         data_frame[desired_labels[1]] = data_frame[dt_label].dt.dayofweek
